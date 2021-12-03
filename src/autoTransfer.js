b565f8b155e6eadbbd0fc1a02818b7a5869ff613
Yaroslav Sorochan <yaroslav.sorochan.i@gmail.com>
2021-12-03 18:26:48 +0200

    dev

--- src/autoTransfer.js
+++ src/autoTransfer.js
@@ -89,7 +89,7 @@ const autoTransfer = async (payerPrivateKey, payerIndex, payers) => {
             const globalProfitStr = globalProfit.toLocaleString()
             const globalProfitPriceStr = (globalProfit * bttPrice).toLocaleString()
             
-            const payerProfitLogStr = `Last ${historyAgeHours} hour(s) profit: ${(payerProfitStr + ' BTT').brightMagenta} (${(payerProfitPriceStr + ' USDT').brightGreen})`
+            const payerProfitLogStr = `, last ${historyAgeHours} hour(s) profit: ${(payerProfitStr + ' BTT').brightMagenta} (${(payerProfitPriceStr + ' USDT').brightGreen})`
             if (payers.length === 1) {
                 log.info(transferLogStr + payerProfitLogStr)
             } else {
