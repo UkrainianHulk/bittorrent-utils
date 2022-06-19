d5ae92a6ad23d4e4317053925ca4dbd48a75dc09
= <=>
2022-06-19 16:44:08 +0300

    2.3.4 - added autoremoving by size

--- src/autoRemove.js
+++ src/autoRemove.js
@@ -6,6 +6,7 @@ const { iteration, bytesToGB, GBtoBytes, msToDHMS, setStringLength } = require('
 
 const torrentsMaxAmount   = config.get('AUTOREMOVE_TORRENTS_MAX_AMOUNT')
 const sizeQuotaPerDriveGB = config.get('AUTOREMOVE_SIZE_QUOTA_PER_DRIVE_GB')
+const torrentMaxSizeGB    = config.get('AUTOREMOVE_TORRENT_MAX_SIZE_GB')
 const minSeedingTimeHours = config.get('AUTOREMOVE_MIN_SEEDING_TIME_HOURS')
 const preventRemoving     = config.get('AUTOREMOVE_PREVENT_REMOVING')
 const intervalSeconds     = config.get('AUTOREMOVE_INTERVAL_SECONDS')
@@ -20,12 +21,12 @@ const getListsPerDrive = (torrentsList) => torrentsList.reduce((acc, torrent) =>
     return acc
 }, {})
 
-const filterBySizeQuota = (torrentsList, sizeQuota) => {
+const findExcessBySizeQuota = (torrentsList, sizeQuota) => {
     const torrentsTotalSize = torrentsList.reduce((acc, torrent) => acc+= torrent.size, 0)
 
     if (torrentsTotalSize > sizeQuota) {
-        const exccess = torrentsTotalSize - sizeQuota
-        const filteredList = []
+        const excessSize = torrentsTotalSize - sizeQuota
+        const excessList = []
         
         do {
             if (torrentsList.length === 0) {
@@ -34,16 +35,16 @@ const filterBySizeQuota = (torrentsList, sizeQuota) => {
             }
             const torrent = torrentsList.pop()
             if (torrent.coefficient === 0) continue
-            filteredList.push(torrent)
-        } while (filteredList.reduce((acc, torrent) => acc += torrent.size, 0) < exccess)
+            excessList.push(torrent)
+        } while (excessList.reduce((acc, torrent) => acc += torrent.size, 0) < excessSize)
         
-        return { filteredList, torrentsTotalSize, exccess }
+        return { excessList, torrentsTotalSize, excessSize }
     }
 
-    return { filteredList: [], torrentsTotalSize }
+    return { excessList: [], torrentsTotalSize }
 }
 
-const filterByMaxAmount = (torrentsList, maxAmount) => {
+const findExcessByMaxAmount = (torrentsList, maxAmount) => {
     if (torrentsList.length > maxAmount) return torrentsList.slice(maxAmount, torrentsList.length)
     else return []
 }
@@ -57,39 +58,51 @@ const autoRemove = async (client) => {
         torrent.seedingTime = torrent.completed && Date.now() - torrent.completed * 1000
     }
 
-    if (torrentsMaxAmount) {
-        const maxAmount = torrentsMaxAmount
-        const filteredList = filterByMaxAmount(sortedList, maxAmount)
-        if (filteredList.length > 0) {
-            log.info(`Client #${client.index}: torrents amount - ${torrentsList.length} of ${maxAmount}, exccess = ${filteredList.length}`)
-        } else {
-            log.debug(`Client #${client.index}: torrents amount - ${torrentsList.length} of ${maxAmount}`)
+    if (torrentMaxSizeGB) {
+        for (let i = 0; i < sortedList.length; i++) {
+            const torrent = sortedList[i]
+            if (torrent.size > GBtoBytes(torrentMaxSizeGB)) {
+                const exceedTorrent = sortedList.splice(i--, 1)[0]
+                exceedTorrent.removeReason = 'MAX_SIZE'
+                removalList.push(exceedTorrent)
+            }
         }
-        removalList.push(...filteredList)
+    }
+
+    if (torrentsMaxAmount) {
+        const excessList = findExcessByMaxAmount(sortedList, torrentsMaxAmount)
+        if (excessList.length > 0) {
+            log.info(`Client #${client.index}: torrents amount - ${sortedList.length} of ${torrentsMaxAmount}, excess = ${excessList.length}`)
+        } else {
+            log.debug(`Client #${client.index}: torrents amount - ${sortedList.length} of ${torrentsMaxAmount}`)
+        }
+        excessList.forEach(t => t.removeReason = 'MAX_AMOUNT')
+        removalList.push(...excessList)
     }
 
     if (sizeQuotaPerDriveGB) {
         const listsPerDrive = getListsPerDrive(sortedList)
         const sizeQuota = GBtoBytes(client.settings.SIZE_QUOTA_PER_DRIVE_GB ? client.settings.SIZE_QUOTA_PER_DRIVE_GB : sizeQuotaPerDriveGB)
-        const filteredList = []
+        const excessList = []
         for (const drive in listsPerDrive) {
-            const torrentsList = listsPerDrive[drive]
-            const { filteredList: filteredDriveList, exccess, torrentsTotalSize } = filterBySizeQuota(torrentsList, sizeQuota)
-            if (filteredDriveList.length > 0) {
-                log.info(`Client #${client.index}, ${drive} - ${bytesToGB(torrentsTotalSize).toFixed(2)}/${bytesToGB(sizeQuota).toFixed(2)}GB, exccess = ${bytesToGB(exccess).toFixed(2)}GB`)
-                filteredList.push(...filteredDriveList)
+            const torrentsListOnDrive = listsPerDrive[drive]
+            const { excessList: excessListOnDrive, excessSize, torrentsTotalSize } = findExcessBySizeQuota(torrentsListOnDrive, sizeQuota)
+            if (excessListOnDrive.length > 0) {
+                log.info(`Client #${client.index}, ${drive} - ${bytesToGB(torrentsTotalSize).toFixed(2)}/${bytesToGB(sizeQuota).toFixed(2)}GB, excess = ${bytesToGB(excessSize).toFixed(2)}GB`)
+                excessList.push(...excessListOnDrive)
             } else {
                 log.debug(`Client #${client.index}, torrents size at drive ${drive} - ${bytesToGB(torrentsTotalSize).toFixed(2)}GB of ${bytesToGB(sizeQuota).toFixed(2)}GB`)
             }
         }
-        removalList.push(...filteredList)
+        excessList.forEach(t => t.removeReason = 'SIZE_QUOTA')
+        removalList.push(...excessList)
     }
 
     const uniqueRemovalList = removalList.filter((item, index, self) => self.findIndex(i => i.hash === item.hash) === index)
-
+    
+    // Prevent removing torrents with low seeding time
     if (minSeedingTimeHours) {
         const minSeedingTimeMS = minSeedingTimeHours * 60 * 60 * 1000
-
         for (let i = 0; i < uniqueRemovalList.length; i++) {
             const torrent = uniqueRemovalList[i]
             if (torrent.seedingTime < minSeedingTimeMS) {
@@ -102,12 +115,15 @@ const autoRemove = async (client) => {
     if (uniqueRemovalList.length > 0) {
         log.info('Torrents to remove:')
         console.table(uniqueRemovalList.map(t => ({
-            name: setStringLength(t.name, 80),
-            drive: path.parse(t.path).root,
-            size: bytesToGB(t.size).toFixed(2) + ' GB',
-            ratio: t.ratio / 1000,
-            seedingTime: msToDHMS(t.seedingTime)
+            'Name': setStringLength(t.name, 65),
+            'Drive': path.parse(t.path).root,
+            'Size': bytesToGB(t.size).toFixed(2) + ' GB',
+            'Ratio': t.ratio / 1000,
+            'Seeding time': msToDHMS(t.seedingTime),
+            'Reason': t.removeReason
         })))
+        
+        // Prevent removing torrents according to settings
         if (!preventRemoving) {
             const uniqueRemovalHashList = uniqueRemovalList.map((item) => item.hash)
             await client.deleteTorrents(uniqueRemovalHashList)
