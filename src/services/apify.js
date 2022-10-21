708973829058a609136817b8d930ca3e70ee933a
UkrainianHulk <yarik.sorochan@ukr.net>
2022-10-21 09:25:24 +0300

    Apify url change

--- src/services/apify.js
+++ src/services/apify.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 export async function getPublicIp() {
-    const res = await fetch('https://api.ipify.org?format=json')
+    const res = await fetch('https://api64.ipify.org?format=json')
     const { ip } = await res.json()
     return ip
 }
