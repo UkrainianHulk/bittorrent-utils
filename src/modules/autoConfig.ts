ba3845d48a1296a33393df39088caecf5e0ab483
UkrainianHulk <yarik.sorochan@ukr.net>
2023-04-03 08:10:09 +0300

    WIP

--- src/modules/autoConfig.ts
+++ src/modules/autoConfig.ts
@@ -1,28 +1,30 @@
-import { setTimeout } from 'node:timers/promises'
-import config from '../libs/config.js'
-import Logger from '../libs/Logger.js'
-import bitTorrent from '../services/bitTorrentClient.js'
-import bitTorrentSpeed from '../services/bitTorrentSpeedClient.js'
+import { setTimeout } from 'node:timers/promises';
+import config from '../libs/config.js';
+import Logger from '../libs/Logger.js';
+import bitTorrent from '../services/bitTorrentClient.js';
+import bitTorrentSpeed from '../services/bitTorrentSpeedClient.js';
 
-const { AUTOCONFIG_SETTINGS } = config
+const { AUTOCONFIG_SETTINGS } = config;
 
-const log = new Logger('autoconfig')
+const log = new Logger('autoconfig');
 
-async function autoConfig() {
-  await bitTorrent.setSettings(AUTOCONFIG_SETTINGS)
-  log.info('BitTorrent settings applied')
-  await bitTorrentSpeed.disableTokensSpending()
-  log.info('Tokens spending disabled')
+async function autoConfig(): Promise<void> {      
+  await bitTorrent.setSettings(AUTOCONFIG_SETTINGS);
+  log.info('BitTorrent settings applied');
+  await bitTorrentSpeed.disableTokensSpending();
+  log.info('Tokens spending disabled');
 }
 
-export async function start() {
+export async function start(): Promise<void> {
   try {
-    await autoConfig()
+    await autoConfig();
   } catch (error) {
-    log.error(error.message)
-    log.debug(error)
-    log.warn('Will retry automatic configuration in 5 sec...')
-    await setTimeout(5000)
-    await start()
+    if (error instanceof Error) {
+      log.error(error.message);
+      if (error.stack != null) log.debug(error.stack);
+    }
+    log.warn('Will retry automatic configuration in 5 sec...');
+    await setTimeout(5000);
+    await start();
   }
 }
